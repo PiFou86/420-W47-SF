5bfbf793de15d7f0b2f012b605de366afb3abc53
Pierre-François Léon <pierrefrancois.leon@gmail.com>
2021-01-11 06:57:08 -0500

    ajout démo

---
+++ Module08_TempsInteractif/DSED_M08_DemoCours/DSED_M08_DemoCours/Entite/Point2d.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSED_M08_DemoCours.Entite
+{
+    public class Point2d
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
