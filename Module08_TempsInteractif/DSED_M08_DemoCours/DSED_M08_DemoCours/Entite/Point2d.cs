696bf62d363c899a8385488140b7086d9bdda2af
Pierre-François Léon <pierrefrancois.leon@gmail.com>
2021-01-11 10:39:20 -0500

    correction zoom navigateur

--- Module08_TempsInteractif/DSED_M08_DemoCours/DSED_M08_DemoCours/Entite/Point2d.cs
+++ Module08_TempsInteractif/DSED_M08_DemoCours/DSED_M08_DemoCours/Entite/Point2d.cs
@@ -7,7 +7,7 @@ namespace DSED_M08_DemoCours.Entite
 {
     public class Point2d
     {
-        public int X { get; set; }
-        public int Y { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
     }
 }
