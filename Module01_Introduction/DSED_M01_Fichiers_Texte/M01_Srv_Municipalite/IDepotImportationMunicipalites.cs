0c93a80afd8e4676b84bb0f75db9225019a71a92
Pierre-François Léon <pierrefrancois.leon@gmail.com>
2021-09-29 13:32:31 -0400

    Corrections diverses

--- Module01_Introduction/DSED_M01_Fichiers_Texte/M01_Srv_Municipalite/IDepotImportationMunicipalites.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace M01_Srv_Municipalite
-{
-    public interface IDepotImportationMunicipalites
-    {
-        IEnumerable<Municipalite> LireMunicipalites();
-    }
-}
