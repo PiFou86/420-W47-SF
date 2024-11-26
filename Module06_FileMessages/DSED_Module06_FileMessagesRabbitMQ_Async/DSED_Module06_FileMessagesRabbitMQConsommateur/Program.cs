e1b5936e87c58233799e528cc8713e18305bc298
Pierre-François Léon <pierrefrancois.leon@gmail.com>
2024-11-26 08:15:44 -0500

    MAJ Lib 7 RabbitMQ

---
+++ Module06_FileMessages/DSED_Module06_FileMessagesRabbitMQ_Async/DSED_Module06_FileMessagesRabbitMQConsommateur/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DSED_Module06_FileMessageProducteur
+{
+    class Program
+    {
+        private static ManualResetEvent waitHandle = new ManualResetEvent(false);
+        static async Task Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connexion = await factory.CreateConnectionAsync())
+            {
+                using (IChannel channel = await connexion.CreateChannelAsync())
+                {
+                    await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false,
+                                                    autoDelete: false, arguments: null
+                    );
+
+                    AsyncEventingBasicConsumer consommateur = new AsyncEventingBasicConsumer(channel);
+                    consommateur.ReceivedAsync += async (model, ea) =>
+                    {
+                        byte[] donnees = ea.Body.ToArray();
+                        string message = Encoding.UTF8.GetString(donnees);
+                        Console.Out.WriteLine(message);
+                        await channel.BasicAckAsync(ea.DeliveryTag, false);
+                    };
+                    await channel.BasicConsumeAsync(queue: "hello",
+                        autoAck: false,
+                        consumer: consommateur
+                    );
+                    waitHandle.WaitOne();
+                }
+            }
+        }
+    }
+}
---
+++ Module06_FileMessages/DSED_Module06_FileMessagesRabbitMQ_Async/DSED_Module06_FileMessagesRabbitMQProducteur/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSED_Module06_FileMessagesRabbitMQ
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connexion = await factory.CreateConnectionAsync())
+            {
+                using (IChannel channel = await connexion.CreateChannelAsync())
+                {
+                    await channel.QueueDeclareAsync(
+                        queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        string message = "mon message - " + Guid.NewGuid();
+                        byte[] body = Encoding.UTF8.GetBytes(message);
+
+                        await channel.BasicPublishAsync(exchange: "", routingKey: "hello", body: body);
+                    }
+                }
+
+            }
+        }
+    }
+}
