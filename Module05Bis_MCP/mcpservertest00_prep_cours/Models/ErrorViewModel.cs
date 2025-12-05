2ea750e451ee326a8b91d2c80416ebd121b94e01
Pierre-François Léon <pierrefrancois.leon@gmail.com>
2025-12-05 07:57:35 -0500

    sd

--- Module05Bis_MCP/mcpservertest00_prep_cours/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace mcpservertest00.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
